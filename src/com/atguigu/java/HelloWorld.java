0913838b19ff4c4ef886fe663f8dd834cf5c7dfa
guoywtw <guoyanwei777@foxmail.com>
2019-06-16 18:13:03 +0800

    新建maven项目

---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.atguigu.java;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println("HelloWorld!");
+        //double Shift  两个Shift
+
+    }
+}
